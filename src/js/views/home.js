e79454ee6c3e86dea430bacdc67f6a6defa5f47b
Leonid ocampo <lnckocampo@yahoo.com>
2023-04-11 01:40:40 +0000

    cambios

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -2,12 +2,44 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
+import { ContactCard } from "../component/ContactCard";
+
 export const Home = () => (
 	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
+		<h1>Contact List</h1>
+		<div className="list-group contact-list">
+			<ContactCard
+			  name={"leo"}
+			  address={"calle numio"}
+			  email={"leo@office.com"}
+			  phone={"+579856321"}
+			  img={rigoImage}
+			/>
+		</div>
+
+		<div className="list-group contact-list">
+			<ContactCard
+			  name={"Sumerce"}
+			  address={"calle numio"}
+			  email={"sumerce@office.com"}
+			  phone={"+5745623125"}
+			  img={rigoImage}
+			/>
+		</div>
+
+		<div className="list-group contact-list">
+			<ContactCard
+			  name={"parce"}
+			  address={"calle rigomontoya"}
+			  email={"parce@office.com"}
+			  phone={"+574523652"}
+			  img={rigoImage}
+			/>
+		</div>
+
+
+
+		
 		<a href="#" className="btn btn-success">
 			If you see this green button, bootstrap is working
 		</a>
